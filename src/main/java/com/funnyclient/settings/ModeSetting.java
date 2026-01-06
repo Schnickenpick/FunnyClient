ae63b6492ef82a40c527baa8f023a1944b23bccb
Schnickenpick <richard.g.raisin@gmail.com>
2026-01-06 17:44:37 +0100

    Add settings system (Setting, BoolSetting, DoubleSetting, ModeSetting)

---
+++ src/main/java/com/funnyclient/settings/ModeSetting.java
@@ -0,0 +1,22 @@
+package com.funnyclient.settings;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ModeSetting extends Setting<String> {
+    private final List<String> modes;
+    
+    public ModeSetting(String name, String defaultValue, String... modes) {
+        super(name, defaultValue);
+        this.modes = Arrays.asList(modes);
+    }
+    
+    public List<String> getModes() {
+        return modes;
+    }
+    
+    public void cycle() {
+        int index = modes.indexOf(getValue());
+        setValue(modes.get((index + 1) % modes.size()));
+    }
+}
